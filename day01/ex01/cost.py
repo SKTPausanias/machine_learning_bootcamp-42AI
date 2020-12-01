38500211066561ca91eeeb314e4b7a8b0a915db2
Miguel Laplana marin <mlaplana@c1r11s6.42madrid.com>
2020-12-01 20:51:49 +0100

    day01 ex03 not working?

---
+++ day01/ex01/cost.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def cost_(y, y_hat):
+	"""Computes the mean squared error of two non-empty numpy.ndarray, without any for loop. The
+	􏰀→ two arrays must have the same dimensions. Args:
+	y: has to be an numpy.ndarray, a vector. y_hat: has to be an numpy.ndarray, a vector. Returns:
+	The mean squared error of the two vectors as a float.
+	None if y or y_hat are empty numpy.ndarray.
+	None if y and y_hat does not share the same dimensions.
+	Raises:
+	This function should not raise any Exceptions.
+	"""
+	if len(y) < 1 or len(y_hat) < 1 or y.shape != y_hat.shape:
+		return None
+	return np.sum((y_hat - y) **2) / float(y.shape[0])
+
+if __name__ == "__main__":
+	X = np.array([0, 15, -9, 7, 12, 3, -21])
+	Y = np.array([2, 14, -13, 5, 12, 4, -19])
+
+	print(cost_(X, Y))
+	print(cost_(X, X))
---
+++ day01/ex03/gradient.py
@@ -0,0 +1,34 @@
+import numpy as np
+from prediction import predict_
+
+def simple_gradient(x, y, theta):
+	"""Computes a gradient vector from three non-empty numpy.ndarray, without any for-loop. The
+	􏰀→ three arrays must have compatible dimensions.
+	Args:
+	x: has to be an numpy.ndarray, a vector of dimension m * 1. y: has to be an numpy.ndarray, a vector of dimension m * 1.
+	theta: has to be an numpy.ndarray, a 2 * 1 vector.
+	Returns:
+	The gradient as a numpy.ndarray, a vector of dimension 2 * 1.
+	None if x, y, or theta are empty numpy.ndarray.
+	None if x, y and theta do not have compatible dimensions.
+	Raises:
+	This function should not raise any Exception.
+	"""
+	if len(x) < 1 or len(y) < 1 or len(theta) < 1 or x.shape != y.shape or theta.shape != (2,):
+		return None
+	gr_vec = np.zeros((2,))
+	y_hat = predict_(x, theta)
+	gr_vec[0] =  np.sum((y_hat - y)) / float(y.shape[0])
+	gr_vec[1] =  np.sum((y_hat - y) * x) / float(y.shape[0])
+	return gr_vec
+
+
+if __name__ == "__main__":
+	x = np.array([12.4956442, 21.5007972, 31.5527382, 48.9145838, 57.5088733])
+	y = np.array([37.4013816, 36.1473236, 45.7655287, 46.6793434, 59.5585554])
+
+	theta1 = np.array([2, 0.7])
+	print(simple_gradient(x, y, theta1))
+
+	theta2 = np.array([1, -0.4])
+	print(simple_gradient(x, y, theta2))
