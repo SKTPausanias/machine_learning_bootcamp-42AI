fb9fb6d44245171a7caaeac8d13f697efba475ab
Miguel Laplana marin <mlaplana@c1r11s6.42madrid.com>
2020-12-12 19:19:26 +0100

    fixing day01

--- day02/ex06/fit.py
+++ day02/ex06/fit.py
@@ -27,8 +27,6 @@ def fit_(x, y, theta, alpha, n_cycles):
 if __name__ == "__main__":
 	x = np.array([[0.2, 2., 20.], [0.4, 4., 40.], [0.6, 6., 60.], [0.8, 8., 80.]])
 	y = np.array([[19.6], [-2.8], [-25.2], [-47.6]])
-	print(x.shape)
-	print(y.shape)
 	theta = np.array([[42.], [1.], [1.], [1.]])
 	# Example 0:
 	theta2 = fit_(x, y, theta,  alpha = 0.0005, n_cycles=42000)
