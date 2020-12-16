fixing normalization in multivariate regression

day02/ex06/fit.py
@@ -1,6 +1,25 @@
 import numpy as np
 from gradient import gradient
 from prediction import predict_
+import matplotlib.pyplot as plt
+
+def mse_(x, theta, y):
+	"""
+	Description:
+	Calculate the MSE between the predicted output and the real output.
+	Args:
+	y: has to be a numpy.ndarray, a vector of dimension m * 1.
+	y_hat: has to be a numpy.ndarray, a vector of dimension m * 1.
+	Returns:
+	mse: has to be a float.
+	None if there is a matching dimension problem.
+	Raises:
+	This function should not raise any Exceptions.
+	"""
+	y_hat = predict_(x, theta)
+	if len(y) < 1 or len(y_hat) < 1:
+		return None
+	return np.sum((y_hat - y) **2) / float(y.shape[0])
 
 def fit_(x, y, theta, alpha, n_cycles):
 	"""
@@ -20,15 +39,37 @@ def fit_(x, y, theta, alpha, n_cycles):
 	"""
 	if len(x) < 1 or len(y) < 1 or len(theta) < 1 or x.shape[0] != y.shape[0] or x is None or y is None:
 		return None
+	#x_norm = np.zeros(x.shape)
+	#i = 0
+	#while i < x.shape[1]:
+	#	x_norm[:,i] = (x[:,i] - x[:,i].mean()) / x[:,i].std()
+	#	i += 1
+	#y_norm = (y - y.mean()) / y.std()
 	for _ in range(n_cycles):
 		theta -= (gradient(x, y, theta) * alpha)
+	#res = theta[0]
+	#i = 1
+	#while i < len(theta):
+	#	res -= (theta[i] * x[:,i - 1].mean() / x[:,i - 1].std())
+	#	i += 1
+	#theta[0] = (res * y.std()) + y.mean()
+	#i = 1
+	#while i < len(theta):
+	#	theta[i] = (theta[i] * y.std() / x[:,i - 1].std())
+	#	i += 1
 	return theta
 
 if __name__ == "__main__":
 	x = np.array([[0.2, 2., 20.], [0.4, 4., 40.], [0.6, 6., 60.], [0.8, 8., 80.]])
 	y = np.array([[19.6], [-2.8], [-25.2], [-47.6]])
 	theta = np.array([[42.], [1.], [1.], [1.]])
+
 	# Example 0:
 	theta2 = fit_(x, y, theta,  alpha = 0.0005, n_cycles=42000)
+
+	print(mse_(x, theta2, y))
 	print(theta2)
-	print(predict_(x, theta2))
+	plt.plot(x, y, '.', color='darkblue')
+	plt.plot(x, predict_(x, theta2), color='dodgerblue')
+	plt.grid()
+	plt.show()

day02/ex07/my_linear_regression.py
@@ -50,15 +50,24 @@ class MyLinearRegression():
 		"""
 		if len(x) < 1 or len(y) < 1 or len(self.thetas) < 1 or x.shape[0] != y.shape[0] or x is None or y is None:
 			return None
-		#x_norm = (x - x.mean()) / x.std()
+		#x_norm = np.zeros(x.shape)
+		#i = 0
+		#while i < x.shape[1]:
+		#	x_norm[:,i] = (x[:,i] - x[:,i].mean()) / x[:,i].std()
+		#	i += 1
 		#y_norm = (y - y.mean()) / y.std()
 		for _ in range(self.max_iter):
 			self.thetas -= (self.gradient(x, y, self.thetas) * self.alpha)
-		#self.thetas[0] = y.mean() + ((self.thetas[0] - (self.thetas[1] * x.mean() / x.std())) * y.std())
-		#self.thetas[1] = (self.thetas[1] * y.std() / x[:,0].std())
-		#self.thetas[2] = (self.thetas[2] * y.std() / x[:,1].std())
-		#self.thetas[3] = (self.thetas[3] * y.std() / x[:,2].std())
-		#self.thetas[4] = (self.thetas[4] * y.std() / x[:,3].std())
+		#res = self.thetas[0]
+		#i = 1
+		#while i < len(self.thetas):
+		#	res -= (self.thetas[i] * x[:,i - 1].mean() / x[:,i - 1].std())
+		#	i += 1
+		#self.thetas[0] = (res * y.std()) + y.mean()
+		#i = 1
+		#while i < len(self.thetas):
+		#	self.thetas[i] = (self.thetas[i] * y.std() / x[:,i - 1].std())
+		#	i += 1
 		return self.thetas
 	
 	def predict_(self, x):
@@ -170,3 +179,8 @@ if __name__ == "__main__":
 	print(mylr.predict_(X))
 	print(mylr.cost_elem_(mylr.predict_(X),Y))
 	print(mylr.cost_(mylr.predict_(X),Y))
+
+	plt.plot(X, Y, '.', color='darkblue')
+	plt.plot(X, mylr.predict_(X), color='dodgerblue')
+	plt.grid()
+	plt.show()

day02/ex08/multivariate_linear_model.py
@@ -7,7 +7,7 @@ class MyLinearRegression():
 	Description:
 	My personnal linear regression class to fit like a boss.
 	"""
-	def __init__(self,  thetas, alpha=1e-5, max_iter=600000):
+	def __init__(self,  thetas, alpha=0.1, max_iter=1000):
 		self.alpha = alpha
 		self.max_iter = max_iter
 		self.thetas = thetas
@@ -50,20 +50,24 @@ class MyLinearRegression():
 		"""
 		if len(x) < 1 or len(y) < 1 or len(self.thetas) < 1 or x.shape[0] != y.shape[0] or x is None or y is None:
 			return None
-		#x_norm = (x - x.mean()) / x.std()
-		#y_norm = (y - y.mean()) / y.std()
+		x_norm = np.zeros(x.shape)
+		i = 0
+		while i < x.shape[1]:
+			x_norm[:,i] = (x[:,i] - x[:,i].mean()) / x[:,i].std()
+			i += 1
+		y_norm = (y - y.mean()) / y.std()
 		for _ in range(self.max_iter):
-			self.thetas -= (self.gradient(x, y, self.thetas) * self.alpha)
-		#res = self.thetas[0]
-		#i = 1
-		#while i < len(self.thetas):
-		#	res -= (self.thetas[i] * x[:,i - 1].mean() / x[:,i - 1].std())
-		#	i += 1
-		#self.thetas[0] = (res * y.std()) + y.mean()
-		#i = 1
-		#while i < len(self.thetas):
-		#	self.thetas[i] = (self.thetas[i] * y.std() / x[:,i - 1].std())
-		#	i += 1
+			self.thetas -= (self.gradient(x_norm, y_norm, self.thetas) * self.alpha)
+		res = self.thetas[0]
+		i = 1
+		while i < len(self.thetas):
+			res -= (self.thetas[i] * x[:,i - 1].mean() / x[:,i - 1].std())
+			i += 1
+		self.thetas[0] = (res * y.std()) + y.mean()
+		i = 1
+		while i < len(self.thetas):
+			self.thetas[i] = (self.thetas[i] * y.std() / x[:,i - 1].std())
+			i += 1
 		return self.thetas
 	
 	def predict_(self, x):
@@ -168,7 +172,7 @@ if __name__ == "__main__":
 	terameters = np.array(data['Terameters']).reshape(-1,1)
 	Sprice = np.array(data['Sell_price']).reshape(-1,1)
 	
-	myLR_age = MyLinearRegression([[1], [1]], 0.0001, 500000)
+	myLR_age = MyLinearRegression([[1], [1]])
 	myLR_age.fit_(age, Sprice)
 	print(myLR_age.mse_(age, Sprice))
 	plt.plot(age, Sprice, '.', markersize=10, color='darkblue', label="Sell price")
@@ -179,7 +183,7 @@ if __name__ == "__main__":
 	plt.grid()
 	plt.show()
 	
-	myLR_thrust = MyLinearRegression([[1], [1]], 0.0001, 500000)
+	myLR_thrust = MyLinearRegression([[1], [1]])
 	myLR_thrust.fit_(thrust_power, Sprice)
 	print(myLR_thrust.mse_(thrust_power, Sprice))
 	plt.plot(thrust_power, Sprice, '.', markersize=10, color='green', label="Sell price")
@@ -190,7 +194,7 @@ if __name__ == "__main__":
 	plt.grid()
 	plt.show()
 	
-	myLR_distance = MyLinearRegression([[1], [1]], 0.0001, 500000)
+	myLR_distance = MyLinearRegression([[1], [1]])
 	myLR_distance.fit_(terameters, Sprice)
 	print(myLR_distance.mse_(terameters, Sprice))
 	plt.plot(terameters, Sprice, '.', markersize=10, color='darkviolet', label="Sell price")