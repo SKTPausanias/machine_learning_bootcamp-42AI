ef873bd6ce8b4314577815d4d189311f8a1cfdfd
Anias <miguellaplanamarin@gmail.com>
2020-12-27 16:25:27 +0100

    day04 ex10

---
+++ day04/ex10/ridge.py
@@ -0,0 +1,173 @@
+import numpy as np
+import pandas as pd
+from data_spliter import data_spliter
+import matplotlib.pyplot as plt
+
+class MyRidge():
+	"""
+	Description:
+	My personnal ridge regression class to fit like a boss.
+	"""
+	def __init__(self, thetas, alpha=0.1, n_cycle=1000, lambda_=0.5):
+		self.alpha = alpha
+		self.n_cycle = n_cycle
+		self.thetas = thetas
+		self.lambda_ = lambda_
+
+	def gradient(self, x, y):
+		"""Computes the regularized linear gradient of three non-empty numpy.ndarray, without any
+		,→ for-loop. The three arrays must have compatible dimensions.
+		Args:
+		y: has to be a numpy.ndarray, a vector of dimension m * 1.
+		x: has to be a numpy.ndarray, a matrix of dimesion m * n.
+		theta: has to be a numpy.ndarray, a vector of dimension n * 1.
+		lambda_: has to be a float.
+		Returns:
+		A numpy.ndarray, a vector of dimension n * 1, containing the results of the formula for all
+		,→ j.
+		None if y, x, or theta are empty numpy.ndarray.
+		None if y, x or theta does not share compatibles dimensions.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		if x.size == 0 or y.size == 0 or self.thetas.size == 0 or x.shape[0] != y.shape[0] or x is None or y is None:
+			return None
+		y_hat = self.predict_(x)
+		thetas2 = np.copy(self.thetas)
+		thetas2[0] = 0
+		gr_vec = (np.matmul(np.transpose(self.add_intercept(x)), (y_hat - y)) + (self.lambda_ * thetas2)) / y.shape[0]
+		return gr_vec
+
+	def fit_(self, x, y):
+		"""
+		Description:
+		Fits the model to the training dataset contained in x and y.
+		Args:
+		x: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+		y: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+		theta: has to be a numpy.ndarray, a vector of dimension 2 * 1.
+		alpha: has to be a float, the learning rate
+		max_iter: has to be an int, the number of iterations done during the gradient descent
+		Returns:
+		new_theta: numpy.ndarray, a vector of dimension 2 * 1.
+		None if there is a matching dimension problem.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		if len(x) < 1 or len(y) < 1 or len(self.thetas) < 1 or x.shape[0] != y.shape[0] or x is None or y is None:
+			return None
+		x_norm = np.zeros(x.shape)
+		i = 0
+		while i < x.shape[1]:
+			x_norm[:,i] = (x[:,i] - x[:,i].mean()) / x[:,i].std()
+			i += 1
+		y_norm = (y - y.mean()) / y.std()
+		for _ in range(self.n_cycle):
+			self.thetas -= (self.gradient(x_norm, y_norm) * self.alpha)
+		res = self.thetas[0]
+		i = 1
+		while i < len(self.thetas):
+			res -= (self.thetas[i] * x[:,i - 1].mean() / x[:,i - 1].std())
+			i += 1
+		self.thetas[0] = (res * y.std()) + y.mean()
+		i = 1
+		while i < len(self.thetas):
+			self.thetas[i] = (self.thetas[i] * y.std() / x[:,i - 1].std())
+			i += 1
+		return self.thetas
+	
+	def predict_(self, x):
+		"""Computes the prediction vector y_hat from two non-empty numpy.ndarray.
+		Args:
+		x: has to be an numpy.ndarray, a matrix of dimension m * n.
+		theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
+		Returns:
+		y_hat as a numpy.ndarray, a vector of dimension m * 1.
+		None if x or theta are empty numpy.ndarray.
+		None if x or theta dimensions are not appropriate.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		if len(x) < 1 or len(self.thetas) < 1 or x is None or self.thetas is None:
+			return None
+		return np.matmul(self.add_intercept(x), self.thetas)
+
+	def add_intercept(self, x):
+		"""Adds a column of 1's to the non-empty numpy.ndarray x.
+		Args:
+		x: has to be an numpy.ndarray, a vector of dimension m * 1.
+		Returns:
+		X as a numpy.ndarray, a vector of dimension m * 2.
+		None if x is not a numpy.ndarray.
+		None if x is a empty numpy.ndarray.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		if len(x) < 1 or type(x) is not np.ndarray:
+			return None
+		return np.c_[np.ones(x.shape[0]), x]
+
+	def reg_cost_(self, y, y_hat):
+		"""Computes the regularized cost of a linear regression model from two non-empty
+		,→ numpy.ndarray, without any for loop. The two arrays must have the same dimensions.
+		Args:
+		y: has to be an numpy.ndarray, a vector of dimension m * 1.
+		y_hat: has to be an numpy.ndarray, a vector of dimension m * 1.
+		theta: has to be a numpy.ndarray, a vector of dimension n * 1.
+		lambda_: has to be a float.
+		Returns:
+		The regularized cost as a float.
+		None if y, y_hat, or theta are empty numpy.ndarray.
+		None if y and y_hat do not share the same dimensions.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		if y.size == 0 or y_hat.size == 0 or self.thetas.size == 0 or y.shape != y_hat.shape:
+			return None
+		thetas2 = np.copy(self.thetas)
+		thetas2[0] = 0
+		return (np.dot((y_hat - y), (y_hat - y)) + (self.lambda_ * (np.dot(thetas2, thetas2)))) / float(y.shape[0] * 2)
+	
+	def mse_(self, y, y_hat):
+		"""
+		Description:
+		Calculate the MSE between the predicted output and the real output.
+		Args:
+		y: has to be a numpy.ndarray, a vector of dimension m * 1.
+		y_hat: has to be a numpy.ndarray, a vector of dimension m * 1.
+		Returns:
+		mse: has to be a float.
+		None if there is a matching dimension problem.
+		Raises:
+		This function should not raise any Exceptions.
+		"""
+		if len(y) < 1 or len(y_hat) < 1 or y.shape != y_hat.shape:
+			return None
+		return np.sum((y_hat - y) **2) / float(y.shape[0])
+
+if __name__ == "__main__":
+	data = pd.read_csv("../resources/spacecraft_data.csv")
+	x = np.array(data[['Age','Thrust_power','Terameters']])
+	y = np.array(data[['Sell_price']])
+	#my_lreg = MyLinearRegression([[1.0], [1.0], [1.0], [1.0]])
+
+	lst = data_spliter(x, y, 0.7)
+	x_train = lst[0]
+	y_train = lst[2]
+	y_train = y_train[:, np.newaxis]
+	x_test = lst[1]
+	y_test = lst[3]
+	y_test = y_test[:, np.newaxis]
+
+	i = 0.0
+	while i <= 1.0:
+		my_lr = MyRidge(np.ones(4).reshape(-1,1), 0.1, 1000, i)
+		my_lr.fit_(x_train, y_train)
+
+		y_hat = my_lr.predict_(x_test)
+
+		print(my_lr.mse_(y_test, y_hat))
+		plt.plot(x_test[:,1], y_test, '.')
+		plt.plot(x_test[:,1], my_lr.predict_(x_test), '.')
+		plt.show()
+		i += 0.1
